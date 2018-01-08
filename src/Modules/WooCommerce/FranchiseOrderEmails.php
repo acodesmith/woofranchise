afbb8716e37949f3caad14ea9b8b4608e3a9e2fd
acodesmith <adam@acodesmith.com>
2018-01-08 09:40:08 -0500

    Emails BCC based on location

---
+++ src/Modules/WooCommerce/FranchiseOrderEmails.php
@@ -0,0 +1,120 @@
+<?php
+
+
+namespace WooFranchise\Modules\WooCommerce;
+
+/**
+ * Class FranchiseOrderEmails
+ * @package WooFranchise\Modules\WooCommerce
+ */
+class FranchiseOrderEmails {
+
+	public function __construct() {
+
+		add_action( 'woocommerce_email_headers', [ $this, 'store_email' ], 10, 2 );
+		add_action( 'woocommerce_email_headers', [ $this, 'district_manager_email' ], 10, 2 );
+	}
+
+	/**
+	 * @param $order_id
+	 * @param null $post_met_key
+	 *
+	 * @return bool|mixed
+	 */
+	public function valid_order_location_post_meta($order_id, $post_met_key = null) {
+
+		// Location ID associated with the Order
+		$order_location_id = get_post_meta( $order_id, 'order_location_id', true );
+		if ( ! $order_location_id )
+			return false;
+
+		// Confirm it's a valid post id
+		$location = get_post( $order_location_id );
+		if ( ! $location )
+			return false;
+
+		// Check for post meta value
+		return get_post_meta( $order_location_id, $post_met_key, true );
+	}
+
+	/**
+	 * @param $headers
+	 * @param $email
+	 *
+	 * @return mixed|string
+	 */
+	public function add_bcc($headers, $email) {
+
+		$pos = strpos( $headers, 'BCC: ' );
+
+		if( false === $pos )
+			$headers .= "BCC: <$email>\r\n";
+		else{
+			$headers = str_replace( 'BCC: ', "BCC: <$email>, ", $headers );
+		}
+
+		return $headers;
+	}
+
+	/**
+	 * Valid the order and the order location.
+	 * Check for the post meta value `wf_store_email`.
+	 * Add the email address as a BCC.
+	 *
+	 * @param $headers
+	 * @param $order_id
+	 *
+	 * @return string
+	 */
+	public function store_email( $headers, $order_id ) {
+
+		$order = wc_get_order( $order_id );
+
+		if ( ! $order ) {
+			return $headers;
+		}
+
+		if ( 'completed' !== $order->get_status() ) {
+			return $headers;
+		}
+
+		$wf_store_email = self::valid_order_location_post_meta($order_id, 'wf_store_email');
+
+		// Add Store Email as BCC
+		if( ! empty( $wf_store_email ) )
+			$headers = self::add_bcc( $headers, $wf_store_email );
+
+		return $headers;
+	}
+
+	/**
+	 * Valid the order and the order location.
+	 * Check for the post meta value `wf_district_manager_email`.
+	 * Add the email address as a BCC.
+	 *
+	 * @param $headers
+	 * @param $order_id
+	 *
+	 * @return mixed|string
+	 */
+	public function district_manager_email( $headers, $order_id ) {
+
+		$order = wc_get_order( $order_id );
+
+		if ( ! $order ) {
+			return $headers;
+		}
+
+		if ( 'completed' !== $order->get_status() ) {
+			return $headers;
+		}
+
+		$wf_district_manager_email = self::valid_order_location_post_meta($order_id, 'wf_district_manager_email');
+
+		// Add District Manager Email as BCC
+		if( ! empty( $wf_district_manager_email ) )
+			$headers = self::add_bcc( $headers, $wf_district_manager_email );
+
+		return $headers;
+	}
+}
